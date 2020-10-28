d90ab6f4aed789abb349ef8b5d082f88d8148d50
Harshil Suthar <sharshil07@gmail.com>
2020-10-28 17:39:39 +0530

    change settings

--- myrest/settings.py
+++ myrest/settings.py
@@ -131,7 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
